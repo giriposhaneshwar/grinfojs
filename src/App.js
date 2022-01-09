7f4e949ec4f84d6cb3843d2f23eab16629ca57d8
Giri Poshaneshwar <egp.designs@gmail.com>
2022-01-09 16:35:47 +0530

    firebase unsued warning fixed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+// eslint-disbale-next-line no-unused-vars
 import firebase from './services/firebaseService';
 
 function App() {
   return (
     <div className="App">
       <h1>Firebase GR-Info APP</h1>
-      
+      <div>
+        Content here
+      </div>
+
     </div>
   );
 }
