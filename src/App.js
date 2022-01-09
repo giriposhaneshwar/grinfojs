2a60ceb4afe136a06dda1f424fd0ec250585f1c9
Giri Poshaneshwar <egp.designs@gmail.com>
2022-01-09 23:08:27 +0530

    user info

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 // eslint-disbale-next-line no-unused-vars
 // import firebase from './services/firebaseService';
-
+import React, { useState, useEffect } from 'react';
 function App() {
+  const [user, setUser] = useState("");
+
+  useEffect(() => {
+    getUserInfo();
+  }, []);
+
+  const getUserInfo = () => {
+    setUser("Giriy");
+  }
+
   return (
     <div className="App">
       <h1>Firebase GR-Info APP</h1>
       <div>
-        Content here new
+        Logged in user: {user}
       </div>
 
     </div>
