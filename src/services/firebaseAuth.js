c2bc96600bcd35651f72fbb278ebd1f8edf81adf
Giri Poshaneshwar <egp.designs@gmail.com>
2022-01-11 18:44:47 +0530

    env ignored

--- src/services/firebaseAuth.js
+++ src/services/firebaseAuth.js
@@ -2,24 +2,21 @@ import firebase from './firebaseService';
 
 
 const auth = firebase.auth();
-const firebaseAuth = () => {
 
-
-    const registerUser = (email, pass) => {
-        return auth.createUserWithEmailAndPassword(email, pass);
-    }
-
-    const signInUser = (email, pass) => {
-        return auth.signInWithEmailAndPassword(email, pass);
-    }
-
-    const loggoutUser = () => {
-        return auth.signOut();
-    }
-
-    return {
-        registerUser, signInUser, loggoutUser
-    }
-
+const registerUser = (email, pass) => {
+    return auth.createUserWithEmailAndPassword(email, pass);
 }
+
+const signInUser = (email, pass) => {
+    return auth.signInWithEmailAndPassword(email, pass);
+}
+
+const loggoutUser = () => {
+    return auth.signOut();
+}
+
+const firebaseAuth = {
+    registerUser, signInUser, loggoutUser
+};
+
 export default firebaseAuth;
